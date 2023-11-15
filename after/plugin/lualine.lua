3cf649aa2930f2f2ec31e2e6ec464713c77641a8
Ryan-Reese <thenewteadrinker@gmail.com>
2023-11-15 09:41:11 +0800

    Changed lualine theme to bamboo

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'rose-pine',
+    theme = 'bamboo',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {

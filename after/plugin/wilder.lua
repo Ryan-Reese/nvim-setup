e232e871ea22e8de98ec84626a853eaa9d519bb8
Ryan Reese <thenewteadrinker@gmail.com>
2023-11-30 23:22:08 +0800

    Fixed bug with search

--- after/plugin/wilder.lua
+++ after/plugin/wilder.lua
@@ -1,25 +1,22 @@
 local wilder = require('wilder')
 
-wilder.setup({modes = {':', '/', '?'}})
---
+wilder.setup({
+    modes = {':', '/', '?'},
+    next_key = '<Tab>',
+    previous_key = '<S-Tab>'})
+
 -- Disable Python remote plugin
 wilder.set_option('use_python_remote_plugin', 0)
 
 wilder.set_option('pipeline', {
-  wilder.branch(
-  wilder.cmdline_pipeline({
-    fuzzy = 1,
-    set_pcre2_pattern = 1,
-  }),
-  wilder.python_search_pipeline({
-    pattern = 'fuzzy',
-  })
-  ),
+    wilder.branch(
+        wilder.cmdline_pipeline(),
+        wilder.search_pipeline()
+    ),
 })
 
 wilder.set_option('renderer', wilder.popupmenu_renderer({
-  highlighter = wilder.basic_highlighter(),
-  reverse = 0,        -- if 1, shows the candidates from bottom to top
-  left = {' ', wilder.popupmenu_devicons()},
-})
-)
+    highlighter = wilder.basic_highlighter(),
+    reverse = 0,        -- if 1, shows the candidates from bottom to top
+    left = {' ', wilder.popupmenu_devicons()}
+}))

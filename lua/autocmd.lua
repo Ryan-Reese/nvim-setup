c2ec5861f48bb041c02430eaf324fd431cc0a61c
Ryan-Reese <thenewteadrinker@gmail.com>
2025-06-25 12:33:18 +0100

    Updated nvim-cmp to blink and changed black to ruff

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -14,18 +14,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     }
   end,
 })
-
--- Not needed with conform?
---[===[
-vim.api.nvim_create_autocmd('BufWritePre', {
-  desc = 'Remove trailing whitespace',
-  group = vim.api.nvim_create_augroup('remove-whitespace', { clear = true }),
-  callback = function()
-    if not vim.o.binary and vim.o.filetype ~= 'markdown' then
-      local current_view = vim.fn.winsaveview()
-      vim.cmd [[keeppatterns %s/\s\+$//e]]
-      vim.fn.winrestview(current_view)
-    end
-  end,
-})
---]===]

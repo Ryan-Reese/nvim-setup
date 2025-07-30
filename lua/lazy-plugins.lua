5bd685d2cd27aa842a49728e6d0128c3c0bd9ccf
Ryan-Reese <thenewteadrinker@gmail.com>
2025-07-30 15:36:17 +0100

    Added guess-indent to modular folder

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -10,16 +10,7 @@
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
   --  Uncomment the following line and add your plugins to `lua/plugins/*.lua` to get going.
   { import = 'plugins' },
   --
